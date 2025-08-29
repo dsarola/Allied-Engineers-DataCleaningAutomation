45d0a17d55348109fe5321a9c6b93e05f2606246
dsarola <dsarola@alliedengineer.com>
2025-08-29 14:35:11 +0530

    Update api.js

--- Frontend_React/src/api.js
+++ Frontend_React/src/api.js
@@ -6,7 +6,7 @@ export const processData = async (file, analyses, thresholds) => {
   formData.append("attenuation_threshold", thresholds.Attenuation || 2);
   formData.append("cpcips_threshold", thresholds.CPCIPS || -1);
 
-  const res = await fetch("https://allied-engineers-datacleaning-automation.onrender.com/process-data/", {
+  const res = await fetch("https://allied-engineers-datacleaningautomation-2i5u.onrender.com/process-data/", {
     method: "POST",
     body: formData,
   });
